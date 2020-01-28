2a5be44413ea57a732966338ca2e5c2e3361d8e5
laalaunch <lastembo@gmail.com>
2020-01-27 20:24:47 -0600

    added setup, location and description codes

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,28 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import com.sun.istack.NotNull;
+
+import javax.persistence.Entity;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @NotNull
+    @Size(max = 75)
+    private String location;
 
+    public Employer(@Size(max = 155, message = "Name must be at least 3 characters long") String location) {
+        this.location = location;
+    }
+
+    public Employer() {}
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
