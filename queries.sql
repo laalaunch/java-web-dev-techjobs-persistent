a7bb04a71bdd996f061dc01e971c9cd0419d551a
laalaunch <lastembo@gmail.com>
2020-01-31 17:23:36 -0600

    assignment 4 parts 1 and 2

--- queries.sql
+++ queries.sql
@@ -1,7 +1,9 @@
 ## Part 1: Test it with SQL
     DESCRIBE job;
 ## Part 2: Test it with SQL
-
+SELECT employer.name
+FROM employer
+WHERE location = "Bozoville USA"
 ## Part 3: Test it with SQL
 
 ## Part 4: Test it with SQL
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -3,7 +3,11 @@ package org.launchcode.javawebdevtechjobspersistent.models;
 import com.sun.istack.NotNull;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Employer extends AbstractEntity {
@@ -11,6 +15,10 @@ public class Employer extends AbstractEntity {
     @NotNull
     @Size(max = 75)
     private String location;
+
+    @OneToMany
+    @JoinColumn
+    private final List<Job> jobs = new ArrayList<>();
 
     public Employer(@Size(max = 155, message = "Name must be at least 3 characters long") String location) {
         this.location = location;
